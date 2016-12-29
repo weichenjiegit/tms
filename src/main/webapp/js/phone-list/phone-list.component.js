d125f67e096d685d191b88de71e91e8943a6beb2
Chenjie Wei <weichenjie2015@gmail.com>
2016-12-29 15:50:36 -0800

    Initial Angular Structure

---
+++ src/main/webapp/js/phone-list/phone-list.component.js
@@ -0,0 +1,21 @@
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('phoneList').
+  component('phoneList', {
+    // Note: The URL is relative to our `index.html` file
+    templateUrl: 'phone-list.template.html',
+    controller: function PhoneListController() {
+      this.phones = [
+        {
+          name: 'Nexus S',
+          snippet: 'Fast just got faster with Nexus S.'
+        }, {
+          name: 'Motorola XOOM? with Wi-Fi',
+          snippet: 'The Next, Next Generation tablet.'
+        }, {
+          name: 'MOTOROLA XOOM?',
+          snippet: 'The Next, Next Generation tablet.'
+        }
+      ];
+    }
+  });
